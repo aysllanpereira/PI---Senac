21b61e765b9272c2ba0fb35b0f7d7787f2bd1d3d
Aysllan Pereira <aysllansantos097@gmail.com>
2025-04-06 00:14:29 -0300

    Alterando o layout

--- myapp-pi/src/Pages/Home/home.jsx
+++ myapp-pi/src/Pages/Home/home.jsx
@@ -1,27 +1,50 @@
-import { Maps } from "../../assets/Components/Maps/maps"
-import { NavBar } from "../../assets/Components/NavBar/navBar"
-import Styles from "../Home/home.module.css"
+import { NavBar } from "../../assets/Components/NavBar/navBar";
+import { Cards } from "../../assets/Components/Cards/cards"
+import Styles from "../Home/home.module.css";
 
 export const Home = () => {
-    return (
-        <>
-            <NavBar />
-            <div className={Styles.home}>
-                <h1>Pesquisar Restaurantes e Lanchonetes</h1>
-                <div>
-                    <form action="#" className={`d-flex align-items-center gap-3`}>
-                        <input
-                            type="text"
-                            name="pesquisa"
-                            id="pes"
-                            className={`form-control`}
-                            placeholder="🔍 Pesquisar Funcionários"
-                        />
-                        <button type="submit" variant="primary">Submit</button>
-                    </form>
-                </div>
-            <Maps />
+  return (
+    <>
+      <NavBar />
+      <div className={Styles.home}>
+        <section>
+        <div>
+          <form action="#" className={`d-flex align-items-center gap-3`}>
+            <input
+              type="text"
+              name="pesquisa"
+              id="pes"
+              className={`form-control`}
+              placeholder="🔍 Buscar restaurantes, culinárias ou pratos típicos"
+            />
+            <button type="submit">
+              <i class="bi bi-filter"></i>
+            </button>
+          </form>
+        </div>
+        </section>
+
+        <section>
+          <div className={Styles.sec}>
+            <div className={Styles.cooking}>
+              <p>
+                <i className="bi bi-egg-fried"></i> Culinária
+              </p>
             </div>
-        </>
-    )
-}
+            <div className={Styles.timeOperation}>
+              <p >
+                <i className="bi bi-clock"></i> Aberto agora
+              </p>
+            </div>
+          </div>
+
+          <div className={Styles.order}>
+            <p>Restaurantes próximos a você</p>
+            <p><i class="bi bi-geo-alt-fill"></i> Ordenado por distância</p>
+          </div>
+        </section>
+      <Cards />
+      </div>
+    </>
+  );
+};
--- myapp-pi/src/assets/Components/NavBar/navBar.jsx
+++ myapp-pi/src/assets/Components/NavBar/navBar.jsx
@@ -6,12 +6,13 @@ export const NavBar = () => {
         <header className={Styles.contHeader}>
             <nav>
                 <div className={Styles.conteiner}>
-                    <h1>Logo</h1>
-                    {/* <div className={Styles.contUl}>
+                    <h1>RestauranteFácil</h1>
+                    <div className={Styles.contNav}>
                         <ul>
-                            <li>Home</li>
+                            <li><i class="bi bi-bookmark"></i> Salvos</li>
+                            <li><i class="bi bi-person-circle"></i> Perfil</li>
                         </ul>
-                    </div> */}
+                    </div>
                 </div>
             </nav>
         </header>
